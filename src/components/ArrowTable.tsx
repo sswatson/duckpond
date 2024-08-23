c168d363e5c50664cd2041e8a68d7bc0369fcc36
Samuel S. Watson <samuel.s.watson@gmail.com>
2024-08-23 10:52:45 -0400

    fix bug with Decimal type

--- src/components/ArrowTable.tsx
+++ src/components/ArrowTable.tsx
@@ -70,8 +70,7 @@ function toString(type: DataType, value: unknown) {
   if (type.constructor.name === "Decimal") {
     if (type.constructor.name === "Decimal") {
       const scale = (type as DataType & { scale: number }).scale;
-
-      if (Array.isArray(value) && value.length === 4) {
+      if (value instanceof Uint32Array && value.length === 4) {
         // Combine the Uint32Array into a 128-bit integer
         let highBits =
           (BigInt(value[3]) << 96n) |
