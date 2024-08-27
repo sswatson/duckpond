ded1f2f2bcc356b5bee431b0943e8132810dc97f
Samuel S. Watson <samuel.s.watson@gmail.com>
2024-08-27 17:25:50 -0400

    fix build-only bug

--- src/components/ArrowTable.tsx
+++ src/components/ArrowTable.tsx
@@ -1,9 +1,9 @@
-import { DataType, Table } from "apache-arrow";
+import { DataType, Decimal, Table } from "apache-arrow";
 
 export default function ArrowTable({
   table,
   rowLimit,
-  incrementRowLimit
+  incrementRowLimit,
 }: {
   table: Table;
   rowLimit: number;
@@ -65,38 +65,34 @@ function toString(type: DataType, value: unknown) {
   if (value === 0 || value === 0n) return "0";
   if (value === false) return "false";
   if (!value) return "";
-  if (!type || !type?.constructor || !type?.constructor?.name)
-    return value.toString();
-  if (type.constructor.name === "Decimal") {
-    if (type.constructor.name === "Decimal") {
-      const scale = (type as DataType & { scale: number }).scale;
-      console.log({value});
-      if (value instanceof Uint32Array && value.length === 4) {
-        // Combine the Uint32Array into a 128-bit integer
-        let highBits =
-          (BigInt(value[3]) << 96n) |
-          (BigInt(value[2]) << 64n) |
-          (BigInt(value[1]) << 32n) |
-          BigInt(value[0]);
+  if (!type) return value.toString();
+  if (Decimal.isDecimal(type)) {
+    const scale = (type as DataType & { scale: number }).scale;
+    if (value instanceof Uint32Array && value.length === 4) {
+      // Combine the Uint32Array into a 128-bit integer
+      let highBits =
+        (BigInt(value[3]) << 96n) |
+        (BigInt(value[2]) << 64n) |
+        (BigInt(value[1]) << 32n) |
+        BigInt(value[0]);
 
-        // Check if the number is negative
-        const isNegative = (value[3] & 0x80000000) !== 0;
+      // Check if the number is negative
+      const isNegative = (value[3] & 0x80000000) !== 0;
 
-        if (isNegative) {
-          // Convert from two's complement to positive magnitude
-          const maxValue = BigInt(1) << 128n; // 2^128
-          highBits = maxValue - highBits;
-          highBits = -highBits;
-        }
-
-        // Apply the scale to get the correct decimal value
-        const decimalValue = highBits / BigInt(10 ** scale);
-
-        // Convert the result to a string
-        return decimalValue.toString();
-      } else {
-        return value.toString();
+      if (isNegative) {
+        // Convert from two's complement to positive magnitude
+        const maxValue = BigInt(1) << 128n; // 2^128
+        highBits = maxValue - highBits;
+        highBits = -highBits;
       }
+
+      // Apply the scale to get the correct decimal value
+      const decimalValue = highBits / BigInt(10 ** scale);
+
+      // Convert the result to a string
+      return decimalValue.toString();
+    } else {
+      return value.toString();
     }
   }
   return value.toString();
